66034d00e6316394ec9224869aa925bd4423db4c
ilker m. sitki <ilkermeliksitki@proton.me>
2024-12-10 17:06:21 +0100

    add cleanup when the radio scripts close
    Changes to be committed:
    	new file:   .gitignore
    	renamed:    check_directory_size.sh -> cleanup.sh
    	modified:   radio_cli.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 DIRECTORY="/home/melik/Documents/projects/radio-cli/saved-streams/"
-# Size limit in bytes 3MB
-SIZE_LIMIT=$((1*1024*1024))
-echo $SIZE_LIMIT
+
+# size limit in bytes 100MB
+SIZE_LIMIT=$((100*1024*1024))
+CURRENT_SIZE=$(du -sb "$DIRECTORY" | cut -f1)
+
+# print the current size
+echo "Current size: " $(($CURRENT_SIZE/ (1024 * 1024))) "MB"
 
 # check if the directory size exceeds the limit
 CURRENT_SIZE=$(du -sb "$DIRECTORY" | cut -f1)
@@ -11,22 +15,22 @@ CURRENT_SIZE=$(du -sb "$DIRECTORY" | cut -f1)
 if [ "$CURRENT_SIZE" -gt "$SIZE_LIMIT" ]; then
     echo "Directory size exceeds the limit. Cleaning up..."
 
-    # Find and delete the oldest .mp3 files until the total size is under the limit
+    # find and delete the oldest .mp3 files until the total size is under the limit
     find "$DIRECTORY" -name "*.mp3" -type f -printf "%T+ %p\n" | sort | while read -r line; do
         FILE=$(echo "$line" | cut -d ' ' -f2-)
         FILE_SIZE=$(stat -c %s "$FILE")
         CURRENT_SIZE=$(($CURRENT_SIZE - $FILE_SIZE))
         
-        # Delete the file if it is older and the size still exceeds the limit
+        # delete the file if it is older and the size still exceeds the limit
         rm "$FILE"
         echo "Deleted: $FILE"
 
-        # If the total size is now under the limit, stop
+        # if the total size is now under the limit, stop
         if [ "$CURRENT_SIZE" -le "$SIZE_LIMIT" ]; then
             break
         fi
     done
 else
-    echo "Directory size is within the limit. No action needed."
+    echo "directory size is within the limit, no action needed."
 fi
 
--- radio_cli.sh
+++ radio_cli.sh
@@ -21,8 +21,22 @@ then
     exit 1
 fi
 
+# trap signals to handle graceful exit
+trap 'echo "Stopping stream..."; ./cleanup.sh; exit 0' SIGINT SIGQUIT SIGTERM
+
 # download and play the radio stream
 echo "Downloading and playing radio stream..."
 
 # fetch the radio stream and save it to a file and play it with mpv
 curl -s "$RADIO_URL" | tee "./saved-streams/$OUTPUT_FILE" | mpv --no-video -
+
+# get the process IDs of curl and mpv
+curl_pid=$!
+mpv_pid=$!
+
+# wait for either the curl or mpv process to finish
+wait $curl_pid
+wait $mpv_pid
+
+# after the stream is finished, clean up the saved streams if necessary
+./cleanup.sh
